5dee3df5f7095c1c1f8f977d316d48db85a1146f
Jaime Sendra <jsendra@autis.es>
2020-09-01 18:03:03 +0200

    test

--- app.py
+++ app.py
@@ -117,10 +117,10 @@ def result():
         return render_template("result.html", sentence=sentence, prediction=prediction)
 
 
-if __name__ == '__main__': 
+# if __name__ == '__main__': 
 
-    app.run(host='104.248.132.63', port=56733)
-#     # app.run(port=8003)
+#     app.run(host='0.0.0.0')
+# #     # app.run(port=8003)
 
 
 
