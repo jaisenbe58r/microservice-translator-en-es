3c521f98ecf47fa2328bb547dfa20413270fcfea
Jaime Sendra <jsendra@autis.es>
2020-09-01 18:56:41 +0200

    nuevos tokenizers

--- app.py
+++ app.py
@@ -30,13 +30,6 @@ NB_PROJ = 8
 DROPOUT_RATE = 0.1
 
 
-def Function_clean(text):
-        # Eliminamos la @ y su mención
-        text = re.sub(r"@[A-Za-z0-9]+", ' ', text)
-        # Eliminamos los links de las URLs
-        text = re.sub(r"https?://[A-Za-z0-9./]+", ' ', text)
-        return text
-        
 processor_en = load('model/processor_en.joblib')
 processor_es = load('model/processor_es.joblib')
 
