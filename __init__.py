0b95269fd7d3d24d6366d016f676d8661a838a84
Jaime Sendra <jsendra@autis.es>
2020-09-01 17:55:17 +0200

    test

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
+import re
+
+
+def Function_clean(text):
+    # Eliminamos la @ y su mención
+    text = re.sub(r"@[A-Za-z0-9]+", ' ', text)
+    # Eliminamos los links de las URLs
+    text = re.sub(r"https?://[A-Za-z0-9./]+", ' ', text)
+    return text
+
 from app import app
 
 app.run(host='0.0.0.0')
