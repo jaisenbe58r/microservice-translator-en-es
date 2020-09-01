1aba437ac8d0575cc73ff477bcc336457443501e
Jaime Sendra <jsendra@autis.es>
2020-09-01 17:14:51 +0200

    test

--- app.py
+++ app.py
@@ -16,13 +16,6 @@ import tensorflow_datasets as tfds
 from mlearner.nlp import Transformer
 from mlearner.nlp import Processor_data
 
-def Function_clean(text):
-        # Eliminamos la @ y su mención
-        text = re.sub(r"@[A-Za-z0-9]+", ' ', text)
-        # Eliminamos los links de las URLs
-        text = re.sub(r"https?://[A-Za-z0-9./]+", ' ', text)
-        return text
-
 from utils.load_model import ValuePredictor
 
 
--- utils/load_model.py
+++ utils/load_model.py
@@ -17,6 +17,13 @@ from mlearner.nlp import Transformer
 from mlearner.nlp import Processor_data
 
 
+def Function_clean(text):
+        # Eliminamos la @ y su mención
+        text = re.sub(r"@[A-Za-z0-9]+", ' ', text)
+        # Eliminamos los links de las URLs
+        text = re.sub(r"https?://[A-Za-z0-9./]+", ' ', text)
+        return text
+
 # Constantes
 MAX_LENGTH = 20
 VOCAB_SIZE_EN = 8198
