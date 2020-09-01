aeff9ff77a7b1b836d7849bbb5311fddbd7fac38
Jaime Sendra <jsendra@autis.es>
2020-09-01 16:55:52 +0200

    test

--- app.py
+++ app.py
@@ -17,14 +17,6 @@ from mlearner.nlp import Transformer
 from mlearner.nlp import Processor_data
 
 
-def Function_clean(text):
-    # Eliminamos la @ y su mención
-    text = re.sub(r"@[A-Za-z0-9]+", ' ', text)
-    # Eliminamos los links de las URLs
-    text = re.sub(r"https?://[A-Za-z0-9./]+", ' ', text)
-    return text
-
-
 #creating instance of the class
 app = Flask(__name__)
 
@@ -52,8 +44,16 @@ def result():
 
 if __name__ == '__main__': 
 
+    
+    def Function_clean(text):
+        # Eliminamos la @ y su mención
+        text = re.sub(r"@[A-Za-z0-9]+", ' ', text)
+        # Eliminamos los links de las URLs
+        text = re.sub(r"https?://[A-Za-z0-9./]+", ' ', text)
+        return text
 
     from utils.load_model import ValuePredictor
+
     app.run(host='0.0.0.0')
     # app.run(port=8003)
 
