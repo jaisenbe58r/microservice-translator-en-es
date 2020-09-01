4abf3bef5e86733ee4a07d503c378bdc319cbb88
Jaime Sendra <jsendra@autis.es>
2020-09-01 17:43:38 +0200

    test

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 
     from utils.load_model import ValuePredictor
     print(ValuePredictor)
-
+    print("*************************************************")
     app.run(host='0.0.0.0')
 #     # app.run(port=8003)
 
